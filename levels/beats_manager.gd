ceb74d4d84da9759b5e5ed44b3f6b6eb7ee4c0bf
BluNuggets <eoczer@gmail.com>
2025-03-31 03:25:51 +0800

    feat(beat_visualizer): add hitmarkers

--- levels/beats_manager.gd
+++ levels/beats_manager.gd
@@ -7,7 +7,8 @@ var _on_beat: bool = false:
 		EventBus.beat_window_changed.emit(val)
 # dependent on song choice, will need smth to handle these
 var _beat_timing_duration: float = 60.0 / 150.0
-var _onbeat_window_duration: float = (60.0 / 150.0) * 1 ## for testing, this should be set like 0.35
+var _onbeat_window_duration: float = (60.0 / 150.0) * 1 ## for testing, this should be set like 0.35\
+var onbeat_offset = 0.0
 
 @onready var _BeatTiming: Timer = $BeatTiming
 @onready var _OnBeatWindow: Timer = $OnBeatWindow
@@ -15,8 +16,7 @@ var _onbeat_window_duration: float = (60.0 / 150.0) * 1 ## for testing, this sho
 
 func _ready() -> void:
 	## play test song
-	$Conductor.play_from_beat(4,0)
-	_TestMetronome.play()
+	$Conductor.play_from_beat(0, onbeat_offset)
 	_BeatTiming.start(_beat_timing_duration)
 	_BeatTiming.timeout.connect(_on_BeatTiming_timeout)
 	_OnBeatWindow.timeout.connect(_on_OnBeatWindow_timeout)
@@ -24,7 +24,6 @@ func _ready() -> void:
 func _on_BeatTiming_timeout() -> void:
 	## start the on-beat window
 	_OnBeatWindow.start(_onbeat_window_duration)
-	_TestMetronome.play()
 	_on_beat = true
 
 func _on_OnBeatWindow_timeout() -> void:
