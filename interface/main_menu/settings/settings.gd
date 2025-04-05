47752af223a01bc1968cc0a4c0bb83c4cf0567ff
04nada <114312907+04nada@users.noreply.github.com>
2025-04-05 16:02:44 +0800

    refactor: rename settings screen to `settings_menu`

--- interface/main_menu/settings/settings.gd
+++
@@ -1,7 +0,0 @@
-extends CenterContainer
-class_name Settings
-
-## Settings stuffs
-@onready var _slider_music: HSlider = %Music
-@onready var _slider_sound: HSlider = %Sound
-@onready var _action_list: VBoxContainer = %ActionList
